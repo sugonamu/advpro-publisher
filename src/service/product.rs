6088bb5ab93e7e016b2c9b4d800bf9becc0bab4e
sugonamu <je0jwff@gmail.com>
2025-03-26 16:13:12 +0700

    Implement publish function in Program service and Program controller.

--- src/service/product.rs
+++ src/service/product.rs
@@ -4,7 +4,6 @@ use rocket::serde::json::Json;
 use bambangshop::{Result, compose_error_response};
 use crate::model::product::Product;
 use crate::repository::product::ProductRepository;
-
 pub struct ProductService;
 
 impl ProductService {
@@ -42,4 +41,18 @@ impl ProductService {
 
         return Ok(Json::from(product));
     }
+
+    pub fn publish(id: usize) -> Result<Product> {
+        let product_opt: Option<Product> = ProductRepository::get_by_id(id);
+        if product_opt.is_none() {
+            return Err(compose_error_response(
+                Status::NotFound,
+                String::from("Product not found.")
+            ));
+        }
+        let product: Product = product_opt.unwrap();
+    
+        NotificationService.notify(&product.product_type, "PROMOTION", product.clone());
+        return Ok(product);
+    }    
 }
